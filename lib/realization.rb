492c5ba871f382e97dbeac2110b379bf3e8629cd
Geoff Ereth <geoff+github@little.fm>
2014-07-04 13:00:15 -0700

    Adding api wrapper to return score for forecat and flights sources

--- lib/realization.rb
+++ lib/realization.rb
@@ -1,31 +1,39 @@
 require 'realization/forecast'
-require 'realization/flight_stats'
+require 'realization/flights'
 
 module Realization
-  class 
-  extend self
-  attr_accessor :channels, :lat, :long, :location
-  
-  def run(location, channels)
-    @location = location
-    @channels = channels
-    @lat = "geoff"
-    from_forecast
-    # @lat, @long = location_to_coordinates(location)
+  class Api
+
+    attr_accessor :lat, :long, :location, :channels
+
+    def initialize(location, channels)
+      @channels   = channels
+      @location   = location
+      @lat, @long = location_to_coordinates(location)
+    end
+
+    def location_to_coordinates(location)
+      {
+        clinton_division: [45.512157, -122.611175]
+      }.fetch(location)
+    end
+
+    # temp             -> very cold | cold | moderate | warm | very warm
+    # precip           -> raining | not raining<optional>
+    # precip add       -> very-light | light | moderate | heavy 
+    # windspeed        -> light-air | etc.. s
+    # sunrise ( 20m )  -> sunrise
+    # sunset  ( 20m )  -> sunset
+    # flights nearby   -> flights | no flights<optional>
+    # flight kind      -> large | small | private 
+    #
+    
+    def to_score
+      forecast = Realization::Forecast.new(lat, long)
+      flights = Realization::Flights.new(lat, long, 5)
+
+      Hash[ *[forecast, flights].map{|obj| obj.to_score.to_a}.flatten].select {|k,v| v}
+    end  
   end
-  
-  def location_to_coordinates(location)
-    {
-      clinton_division: [45.512924, -122.5842]
-    }
-  end
-  
-  def from_forecast
-    lat
-  end
-  
-  def from_flight_stats
-  end
-  
 end
 
