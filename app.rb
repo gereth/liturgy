df5d3ac108e6fa6b9baf50d5640a6266a9d971b5
Geoff Ereth <geoff+github@little.fm>
2014-07-13 16:57:38 -0600

    Move to_json in realization

--- app.rb
+++ app.rb
@@ -1,30 +1,30 @@
 class App < Sinatra::Base
-  
+
   use Rack::Auth::Basic, "Restricted Area" do |user, key|
     user == 'api' and key == ENV['API_KEY']
   end
-  
+
   configure do
     set :cache, Sinatra::Cache::RedisStore.new(url: (ENV['REDISTOGO_URL'] || 'redis://127.0.0.1:6379'))
   end
-  
+
   before do
     setup_params
   end
-  
+
   get '/api/realization.json' do
     content_type :json
     settings.cache.fetch(cache_name, expires_in: 60) do
-      Realization::Api.new(@location, @channels).to_score.to_json
+      Realization::Api.new(@location, @channels).score!
     end
   end
-  
+
   protected
-  
+
   def cache_name
     ["store", @location, @channels].flatten.join("-")
   end
-  
+
   def setup_params
     %w( location lat long channels ).each do |name|
       instance_variable_set(:"@#{name}", params.fetch(name)) if params[name]
